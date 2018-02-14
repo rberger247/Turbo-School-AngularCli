0be2b54e66ecd990fcf7a6874c1114de50dd21ee
Rafael Berger <Rafiberger613@gmail.com>
2018-02-14 15:21:33 -0500

    initial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,11 @@ import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { Subject } from 'rxjs/Subject';
 import { AppComponent }  from './app.component';
+import { PaymentComponent } from './donor/payment/payment.component';
 
 @NgModule({
   imports:      [ BrowserModule ],
-  declarations: [ AppComponent ],
+  declarations: [ AppComponent, PaymentComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
